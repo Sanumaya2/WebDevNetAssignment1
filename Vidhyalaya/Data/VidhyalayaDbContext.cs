7f24b837a4866bc5e8b722a58bf2fd48fb573df5
Sanumaya2 <goritara23456@gmail.com>
2024-05-07 04:28:41 -0700

    Dbcontext

--- Vidhyalaya/Data/VidhyalayaDbContext.cs
+++ Vidhyalaya/Data/VidhyalayaDbContext.cs
@@ -5,11 +5,8 @@ public class VidhyalayaDbContext : DbContext
 {
     public DbSet<Grade> Grades { get; set; }
     public DbSet<Student> Students { get; set; }
-
-     protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Grade>().HasKey(g => g.Label);
-    }
+    public DbSet<Guardian> GuardianDetails{ get; set; }
+  
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=Vidhyalaya.db");
