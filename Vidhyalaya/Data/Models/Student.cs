46a5f22348183e0f8680140b83b57cd5d6cc174f
Sanumaya2 <goritara23456@gmail.com>
2024-05-09 02:34:49 -0700

    Add FK

--- Vidhyalaya/Data/Models/Student.cs
+++ Vidhyalaya/Data/Models/Student.cs
@@ -10,6 +10,9 @@ public class Student
     public DateTime Dob { get; set; }
    public string? Photo {get ; set ;}
    public bool IsActive {get; set ;}
+    public int? GradeLabel { get; set; } 
+    public Grade? Grade { get; set; } 
+
     
 
 
